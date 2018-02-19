08c344b72e69276bfafc2aba926730b4a20a129a
Robert John Millano <bemanj87@gmail.com>
2018-02-19 22:51:07 +0800

    test 2

--- InteractiveRebase/InteractiveRebase/Class1.vb
+++ InteractiveRebase/InteractiveRebase/Class1.vb
@@ -19,16 +19,6 @@
         End Set
     End Property
 
-    Private _lastname As String
-    Public Property LastName() As String
-        Get
-            Return _lastname
-        End Get
-        Set(ByVal value As String)
-            _lastname = value
-        End Set
-    End Property
-
     Function DisplayName() As String
         Dim prefix As String
         prefix = "Name: "
