19d4ad44ba0014144ff5e9876a60ec0de4749abf
Robert John Millano <bemanj87@gmail.com>
2018-02-19 19:47:28 +0800

    added new class

---
+++ InteractiveRebase/InteractiveRebase/Class1.vb
@@ -0,0 +1,18 @@
+﻿Public NotInheritable Class SampleClass
+    Private _firstName As String
+    Public Property FirstName() As String
+        Get
+            Return _firstName
+        End Get
+        Set(ByVal value As String)
+            _firstName = value
+        End Set
+    End Property
+
+    Function DisplayName() As String
+        Dim prefix As String
+        prefix = "Name: "
+        Return prefix & _firstName
+    End Function
+
+End Class
